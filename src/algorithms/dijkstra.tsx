e15ddb0f4844b1f25f09b7ccb31e37e1516a3ade
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2021-07-23 02:12:04 -0700

    Refactor dijkstra's algorithm

--- src/algorithms/dijkstra.tsx
+++ src/algorithms/dijkstra.tsx
@@ -1,70 +1,38 @@
 /* eslint-disable no-param-reassign */
 import { SquareType } from '../types/SquareTypes';
 import { Grid, Node } from '../types/VisualizerTypes';
+import {
+  flattenNodes,
+  getPath,
+  getUnvisitedNeighbors,
+  sortNodes,
+} from '../utils/algorithmUtils';
 
-function sortNodesByDistance(unvisitedNodes: Node[]): void {
-  unvisitedNodes.sort((nodeA, nodeB) => nodeA.dist - nodeB.dist);
-}
-
-function getUnvisitedNeighbors(node: Node, nodes: Node[][]): Node[] {
-  const neighbors = [];
-  const { col, row } = node;
-  if (row > 0) neighbors.push(nodes[row - 1][col]);
-  if (row < nodes.length - 1) neighbors.push(nodes[row + 1][col]);
-  if (col > 0) neighbors.push(nodes[row][col - 1]);
-  if (col < nodes[0].length - 1) neighbors.push(nodes[row][col + 1]);
-  return neighbors.filter((neighbor) => neighbor.type !== SquareType.Visited);
-}
-
-function updateUnvisitedNeighbors(node: Node, nodes: Node[][]): void {
-  const unvisitedNeighbors: Node[] = getUnvisitedNeighbors(node, nodes);
+function updateNeighbors(curr: Node, nodes: Node[][]): void {
+  const unvisitedNeighbors: Node[] = getUnvisitedNeighbors(curr, nodes);
   unvisitedNeighbors.forEach((neighbor) => {
-    neighbor.dist = node.dist + 1;
-    neighbor.prev = node;
+    neighbor.dist = curr.dist + 1;
+    neighbor.prev = curr;
   });
 }
 
-function getNodes(nodes: Node[][]): Node[] {
-  const flat: Node[] = [];
-  for (let r = 0; r < nodes.length; r += 1) {
-    for (let c = 0; c < nodes[0].length; c += 1) {
-      flat.push(nodes[r][c]);
-    }
-  }
-  return flat;
-}
-
-function getNodesInShortestPathOrder(target: Node): Node[] {
-  const nodesInShortestPathOrder: Node[] = [];
-  let currentNode: Node = target;
-  while (currentNode !== null) {
-    nodesInShortestPathOrder.unshift(currentNode);
-    if (currentNode.prev != null) {
-      currentNode = currentNode.prev;
-    } else {
-      break;
-    }
-  }
-  return nodesInShortestPathOrder;
-}
-
 export default function dijkstra(grid: Grid): [Node[], Node[]] {
-  grid.source.dist = 0;
-  const visitedNodesInOrder: Node[] = [];
-  const unvisitedNodes: Node[] = getNodes(grid.nodes);
-  while (unvisitedNodes.length) {
-    sortNodesByDistance(unvisitedNodes);
-    const closestNode: Node = unvisitedNodes[0];
-    unvisitedNodes.shift();
+  const unvisited: Node[] = flattenNodes(grid.nodes);
+  const visited: Node[] = [];
 
-    if (closestNode.type !== SquareType.Wall) {
-      if (closestNode.dist === Infinity) return [visitedNodesInOrder, []];
-      closestNode.type = SquareType.Visited;
-      visitedNodesInOrder.push(closestNode);
-      if (closestNode === grid.target) break;
-      updateUnvisitedNeighbors(closestNode, grid.nodes);
+  grid.source.dist = 0;
+  while (unvisited.length > 0) {
+    sortNodes(unvisited);
+    const closest: Node | undefined = unvisited.shift();
+
+    if (closest && closest.type !== SquareType.Wall) {
+      if (closest.dist === Infinity) return [visited, []];
+      closest.type = SquareType.Visited;
+      visited.push(closest);
+      if (closest === grid.target) break;
+      updateNeighbors(closest, grid.nodes);
     }
   }
 
-  return [visitedNodesInOrder, getNodesInShortestPathOrder(grid.target)];
+  return [visited, getPath(grid.target)];
 }
---
+++ src/utils/algorithmUtils.tsx
@@ -0,0 +1,37 @@
+import { Node } from '../types/VisualizerTypes';
+import { SquareType } from '../types/SquareTypes';
+
+export function flattenNodes(nodes: Node[][]): Node[] {
+  const flat: Node[] = [];
+  for (let r = 0; r < nodes.length; r += 1) {
+    for (let c = 0; c < nodes[0].length; c += 1) {
+      flat.push(nodes[r][c]);
+    }
+  }
+  return flat;
+}
+
+export function sortNodes(nodes: Node[]): void {
+  nodes.sort((a, b) => a.dist - b.dist);
+}
+
+export function getPath(target: Node): Node[] {
+  const path: Node[] = [];
+  let curr: Node | null = target;
+  while (curr !== null) {
+    path.unshift(curr);
+    curr = curr.prev;
+  }
+  return path;
+}
+
+export function getUnvisitedNeighbors(node: Node, nodes: Node[][]): Node[] {
+  const neighbors = [];
+  const { col, row } = node;
+
+  if (row > 0) neighbors.push(nodes[row - 1][col]);
+  if (col > 0) neighbors.push(nodes[row][col - 1]);
+  if (row < nodes.length - 1) neighbors.push(nodes[row + 1][col]);
+  if (col < nodes[0].length - 1) neighbors.push(nodes[row][col + 1]);
+  return neighbors.filter((neighbor) => neighbor.type !== SquareType.Visited);
+}
